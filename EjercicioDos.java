8004a2cbf1ceaaa32a4bc89245e20a672d42fc2b
maxidelarosa <31898646+maxidelarosa@users.noreply.github.com>
2017-09-13 17:01:08 -0300

    commit 5
    ejercicio resuelto escanea el nombre ingresado

--- EjercicioDos.java
+++ EjercicioDos.java
@@ -3,19 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejerciciodos;
+package ejercicioDos;
+
+import java.util.Scanner;
 
 /**
  *
  * @author alumno
  */
-public class EjercicioDos {
+public class Ejerciciodos {
 
     /**
      * @param args the command line arguments
+     * se debe pedir un nombre y se debe mostrar el mismo nombre
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       // TODO code application logic here
+        String nombre;
+        Scanner minuevoscanner = new Scanner(System.in);
+        System.out.println("por ingrese su nombre");
+        nombre =minuevoscanner.nextLine();
+        System.out.println("el nombre ingresado es"+ nombre );
+        
+        
+        
     }
     
 }
