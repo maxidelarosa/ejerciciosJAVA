8f4787b8c564be833204a66a0be550f766bf32e5
maxidelarosa <31898646+maxidelarosa@users.noreply.github.com>
2017-09-13 17:51:19 -0300

    Add files via upload

---
+++ EjercicioCuatro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocuatro;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioCuatro {
+
+    /**
+     * @param args the command line arguments
+     * al ingresar una edad y el nombre se debe mostrar el sgte mensaje
+     * usted se llama xxx y tiene xxx años de edad
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
